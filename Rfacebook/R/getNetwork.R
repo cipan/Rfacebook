e30602e32151a1f612ad5fb450c15fa2178bbb11
Cole Campton <camptonc@yahoo.com>
2014-12-08 17:43:36 -0800

    Update getNetwork.R

--- Rfacebook/R/getNetwork.R
+++ Rfacebook/R/getNetwork.R
@@ -1,7 +1,7 @@
 #' @rdname getNetwork
 #' @export
 #'
-#' @title 
+#' @title
 #' Extract network of friends of authenticated user
 #'
 #' @description
@@ -10,7 +10,7 @@
 #' of the authenticated user.
 #'
 #' @details
-#' 
+#'
 #' This function requires the use of an OAuth token with extended permissions.
 #'
 #' @author
@@ -18,10 +18,10 @@
 #' @seealso \code{\link{getFriends}}, \code{\link{fbOAuth}}
 #'
 #' @param token Either a temporary access token created at
-#' \url{https://developers.facebook.com/tools/explorer} or the OAuth token 
+#' \url{https://developers.facebook.com/tools/explorer} or the OAuth token
 #' created with \code{fbOAuth}.
 #'
-#' @param format Either "edgelist" for a list of edges in the network or 
+#' @param format Either "edgelist" for a list of edges in the network or
 #' "adj.matrix" for an adjacenty matrix of dimensions (n x n), with n being
 #' the number of friends, and 0 or 1 indicating whether friend i is also friends
 #' with friend j.
@@ -32,46 +32,39 @@
 #' @examples \dontrun{
 #' ## See examples for fbOAuth to know how token was created.
 #' ## Getting my network of friends
-#'  load("fb_oauth")
-#'  mat <- getNetwork(token=fb_oauth, format="adj.matrix")
-#'  library(igraph)
-#'  network <- graph.adjacency(mat, mode="undirected")
-#'  pdf("network_plot.pdf")
-#'  plot(network)
-#'  dev.off()
+#' load("fb_oauth")
+#' mat <- getNetwork(token=fb_oauth, format="adj.matrix")
+#' library(igraph)
+#' network <- graph.adjacency(mat, mode="undirected")
+#' pdf("network_plot.pdf")
+#' plot(network)
+#' dev.off()
 #' }
 #'
-
 getNetwork <- function(token, format='edgelist', verbose=TRUE){
-	if (format %in% c("edgelist", "adj.matrix") == FALSE){
-		stop("format not recognized. Choose either 'edgelist' or 'adj.matrix'.")
-	}
-	friends <- getFriends(token=token, simplify=TRUE)
-	edge.list <- NULL
-	n <- length(friends$id)
-	if (verbose==TRUE){ pb <- txtProgressBar(min=1,max=n, style=3) }
-	for (i in 1:n){
-		query <- paste0("https://graph.facebook.com/", friends$id[i],"?fields=context.fields(mutual_friends)")
-		content <- callAPI(query, token)
-		mutual.friends <- unlist(lapply(content[[1]], '[[', 'name'))
-		for (friend in mutual.friends){
-			edge.list <- rbind(edge.list, c(friends$name[i], friend))
-		}
-		if (verbose==TRUE){ setTxtProgressBar(pb, i) }
-	}
-	if (format=="adj.matrix"){
-		adj.matrix <- matrix(
-			unlist(lapply(friends$name, function(x) 
-			friends$name %in% edge.list[edge.list[,1]==x,2])),
-			ncol=n, nrow=n)
-		dimnames(adj.matrix) <- list(friends$name, friends$name)
-		
-	}
-	if (format=="edgelist"){ return(edge.list) }
-	if (format=="adj.matrix") { return(adj.matrix) }
+if (format %in% c("edgelist", "adj.matrix") == FALSE){
+stop("format not recognized. Choose either 'edgelist' or 'adj.matrix'.")
 }
-
-
-
-
-
+friends <- getFriends(token=token, simplify=TRUE)
+edge.list <- NULL
+n <- length(friends$id)
+if (verbose==TRUE){ pb <- txtProgressBar(min=1,max=n, style=3) }
+for (i in 1:n){
+query <- paste0("https://graph.facebook.com/", friends$id[i], "?fields=context.fields(mutual_friends)")
+content <- callAPI(query, token)
+mutual.friends <- unlist(lapply(content[[1]], '[[', 'name'))
+for (friend in mutual.friends){
+edge.list <- rbind(edge.list, c(friends$name[i], friend))
+}
+if (verbose==TRUE){ setTxtProgressBar(pb, i) }
+}
+if (format=="adj.matrix"){
+adj.matrix <- matrix(
+unlist(lapply(friends$name, function(x)
+friends$name %in% edge.list[edge.list[,1]==x,2])),
+ncol=n, nrow=n)
+dimnames(adj.matrix) <- list(friends$name, friends$name)
+}
+if (format=="edgelist"){ return(edge.list) }
+if (format=="adj.matrix") { return(adj.matrix) }
+}
